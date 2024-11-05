f4a8bd3b55ada2f5a5a2c27a923e2e2491b11ff5
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-05 19:49:38 +0100

    debut d'ajout de la partie genetic

--- Cell.cs
+++ Cell.cs
@@ -11,18 +11,57 @@ namespace CelluleMutante2
     {
         public int size;
         public Color color;
+        public string genetic;
         private Random rnd = new Random();
 
         public Cell()
         {
             size = 10;
             color = Color.Black;
+            genetic = "A";
         }
 
         public void Mutation()
         {
+
+            if (genetic.Length > 3)
+            {
+
+                int i = 0;
+                string chaine = "";
+
+                while (i < genetic.Length - 3)
+                {
+                    chaine = genetic.Substring(i, 3);
+                    switch (chaine)
+                    {
+                        case "TGT":
+                            color = Color.FromArgb(0, 0, 0);
+                            break;
+                        case "ATT":
+                            color = Color.FromArgb(0, 0, 255);
+                            break;
+                        case "CTC":
+                            color = Color.FromArgb(0, 255, 255);
+                            break;
+                        case "ACT":
+                            color = Color.FromArgb(255, 0, 255);
+                            break;
+                        case "GTC":
+                            color = Color.FromArgb(255, 165, 0);
+                            break;
+                        case "GAA":
+                            color = Color.FromArgb(0, 255, 0);
+                            break;
+                    }
+                    i++;
+                }
+            }
+
+
+            
             size += 5;
-            color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            
         }
     }
 }
